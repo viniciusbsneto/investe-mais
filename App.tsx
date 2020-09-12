52f88381d248db29709c2ed32508944fdf420924
Vinícius Neto <viniciusbsneto@gmail.com>
2020-09-12 16:47:07 -0300

    feat: add stack navigation

--- App.tsx
+++ App.tsx
@@ -1,15 +1,18 @@
 import { AppLoading } from 'expo';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import SignIn from './src/pages/SignIn';
 import { useFonts } from 'expo-font';
 import { RobotoSlab_400Regular, RobotoSlab_700Bold } from '@expo-google-fonts/roboto-slab';
 import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import AppStack from './src/routes/AppStack';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
     RobotoSlab_400Regular,
     RobotoSlab_700Bold,
+    Roboto_400Regular,
+    Roboto_700Bold
   });
 
   if (!fontsLoaded) {
@@ -17,7 +20,7 @@ export default function App() {
   } else {
     return (
       <>
-        <SignIn />
+        <AppStack />
         <StatusBar style="auto" />
       </>
     );
---
+++ src/routes/AppStack.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SignIn from '../pages/SignIn';
+
+const { Navigator, Screen } = createStackNavigator();
+
+function AppStack() {
+  return (
+    <NavigationContainer>
+      <Navigator screenOptions={{ headerShown: false }}>
+        <Screen name="SignIn" component={SignIn} />
+      </Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default AppStack;
