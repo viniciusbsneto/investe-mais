656b98993ab264d132d18bbeda95c6bd60464dc9
Vinícius Neto <viniciusbsneto@gmail.com>
2020-09-13 22:36:49 -0300

    feat: add mocked sigIn method

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,21 +3,25 @@ import { View, Image, TouchableOpacity, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
 
+import { useAuth } from '../../hooks/auth';
+
 import styles from './styles';
 
 import logo from '../../../assets/logo.png';
 
 const SignIn: React.FC = () => {
   const { navigate } = useNavigation();
+  const { signIn } = useAuth();
 
-  const handleNavigateToDashboardPage = useCallback(() => {
+  const handleSignIn = useCallback(async () => {
+    await signIn();
     navigate('Dashboard');
   }, []);
 
   return (
     <View style={styles.container}>
       <Image source={logo} style={styles.logo} />
-      <RectButton style={styles.button} onPress={handleNavigateToDashboardPage}>
+      <RectButton style={styles.button} onPress={handleSignIn}>
         <Text style={styles.buttonText}>Entrar</Text>
       </RectButton>
     </View>
