73a2c01f696fb0c9385e74265c7f33630091baa1
Vinícius Neto <viniciusbsneto@gmail.com>
2020-09-13 22:38:00 -0300

    feat: change incentive actions page to consume API

--- src/pages/IncentiveActions/index.tsx
+++ src/pages/IncentiveActions/index.tsx
@@ -1,34 +1,57 @@
 import React from 'react';
 import { View, Image, Text } from 'react-native';
-import { RectButton } from 'react-native-gesture-handler';
+import { RectButton, ScrollView } from 'react-native-gesture-handler';
 
 import PageHeader from '../../components/PageHeader';
 
 import backgroundIncentiveActions from '../../assets/images/backgroundIncentiveActions.png';
 
+import { useAuth } from '../../hooks/auth';
+
 import styles from './styles';
 
 const IncentiveActions: React.FC = () => {
+  const { user } = useAuth();
+  
   return (
     <View style={styles.container}>
       <PageHeader title="Ações de incentivo Safra" />
-      <Image
-        style={styles.banner}
-        source={backgroundIncentiveActions}
-      />
-      <View>
-        <Text style={styles.paragraph}>
-          A cada 1 dólar investido com o Invest+ ganhe
-          uma SafraCoin para que você possa investir mais ainda
-        </Text>
-      </View>
-      <View style={styles.buttonsContainer}>
-        <RectButton style={styles.button}>
-          <Text style={styles.buttonText}>
-            
+      <ScrollView
+        style={styles.scrollPage}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        <View style={styles.innerContainer}>
+          <Image
+            style={styles.banner}
+            source={backgroundIncentiveActions}
+          />
+          <View>
+            <Text style={styles.paragraph}>
+              A cada 1 dólar investido com o Invest+ ganhe
+              uma SafraCoin para que você possa investir mais ainda
+            </Text>
+          </View>
+          <Text style={styles.totalCoins}>
+            Total de SafraCoins acumulados
           </Text>
-        </RectButton>
-      </View>
+          <Text style={styles.totalCoinsValue}>{user.SafraCoins}</Text>
+          <View style={styles.buttonsContainer}>
+            <RectButton style={styles.button}>
+              <Text style={styles.buttonText}>
+                Trocar SafraCoins
+              </Text>
+            </RectButton>
+            <RectButton style={styles.button}>
+              <Text style={styles.buttonText}>
+                Extrato SafraCoins
+              </Text>
+            </RectButton>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
--- src/pages/IncentiveActions/styles.ts
+++ src/pages/IncentiveActions/styles.ts
@@ -6,29 +6,77 @@ const styles = StyleSheet.create({
     backgroundColor: '#f0f0f7',
   },
 
+  scrollPage: {
+    marginTop: -60,
+  },
+
+  innerContainer: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#e6e6f0',
+    borderRadius: 8,
+    marginBottom: 16,
+    overflow: 'hidden',
+  },
+
   banner: {
     width: '100%',
+    height: 300,
     resizeMode: "contain",
+    padding: 24,
   },
 
   paragraph: {
-
+    marginHorizontal: 24,
+    fontFamily: 'Roboto_400Regular',
+    fontSize: 16,
+    lineHeight: 24,
+    color: '#6a6180',
+    textAlign: "justify",
   },
 
   totalCoins: {
+    marginTop: 8,
+    marginHorizontal: 24,
+    fontFamily: 'RobotoSlab_700Bold',
+    fontSize: 18,
+    lineHeight: 32,
+    color: '#6a6180',
+    textAlign: 'center',
+  },
 
+  totalCoinsValue: {
+    marginTop: 8,
+    marginHorizontal: 24,
+    fontFamily: 'RobotoSlab_700Bold',
+    fontSize: 32,
+    lineHeight: 32,
+    color: '#6a6180',
+    textAlign: 'center',
   },
 
   buttonsContainer: {
-
+    flexDirection: 'row',
+    padding: 24,
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
 
   button: {
-
+    backgroundColor: '#d4ad68',
+    width: 130,
+    height: 60,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 10,
   },
 
   buttonText: {
-
+    color: '#fff',
+    fontFamily: 'RobotoSlab_700Bold',
+    fontSize: 14,
+    textAlign: 'center',
   },
 });
 
